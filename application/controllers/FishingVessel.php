743957eca297d72f945931e1753338a5aeb50e39
aeksorra <gsorra@gmail.com>
2018-05-03 14:07:47 +0700

    สร้าง controller สำหรับจัดการข้อมูลเรือประมง

---
+++ application/controllers/FishingVessel.php
@@ -0,0 +1,23 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class FishingVessel extends CI_Controller {
+
+    public function index()
+    {
+        echo 'Fishing Vessel';
+    }
+
+    public function all()
+    {
+        echo 'Show all Vessel';
+    }
+
+}
+
+/* End of file FishingVessel.php */
+
+
+?>
