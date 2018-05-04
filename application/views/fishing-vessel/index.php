308bdc863e45fbdc99736fa96d2403562ea1daa7
aeksorra <gsorra@gmail.com>
2018-05-04 10:46:17 +0700

    แก้ไขเล็กน้อย

--- application/views/fishing-vessel/index.php
+++ application/views/fishing-vessel/index.php
@@ -16,40 +16,35 @@
             <div class="card-header">
             ชื่อเรือ
         </div>
-  <div class="card-body">
-    <h5 class="card-title">ประเทศ</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      </div>
-</div>
-    
+        <div class="card-body">
+            <h5 class="card-title">ประเทศ</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        </div>    
     </div>
 
     <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
         <div class="card">
             <div class="card-header">
             ชื่อเรือ
+            </div>
+        <div class="card-body">
+            <h5 class="card-title">ประเทศ</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
         </div>
-  <div class="card-body">
-    <h5 class="card-title">ประเทศ</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      </div>
-</div>
-    
+        </div>    
     </div>
 
     <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
         <div class="card">
             <div class="card-header">
             ชื่อเรือ
+            </div>
+        <div class="card-body">
+            <h5 class="card-title">ประเทศ</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
         </div>
-  <div class="card-body">
-    <h5 class="card-title">ประเทศ</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      </div>
-</div>
-    
+        </div>    
     </div>
-
-
 
 </div>
