bc4d8eb084e06e4da544f8648db19e35c9133d6e
aeksorra <gsorra@gmail.com>
2018-05-07 14:55:11 +0700

    แก้ตาม

--- application/controllers/FishingVessel.php
+++ application/controllers/FishingVessel.php
@@ -28,6 +28,11 @@ class FishingVessel extends CI_Controller
     }
     public function create()
     {
+        $config['upload_path'] = './upload/';
+        $config['allowed_types'] = 'gif|jpg|png|jpeg';
+        // $config['max_size'] = 100;
+        // $config['max_width'] = 1024;
+        $this->load->library('upload', $config);
         $this->load->library('form_validation');
         $this->form_validation->set_rules('vesselName', 'ชื่อเรือ', 'required|max_length[10]', array('required' => 'ช่วยกรอกชื่อสำเภาด้วยนะออเจ้า' ));
         if ($this->form_validation->run() == false) 
@@ -42,9 +47,26 @@ class FishingVessel extends CI_Controller
         }
         else 
         {
-            $this->load->model('fishingvessel_model');
-            $this->fishingvessel_model->save_new_vessel();
-            redirect('fishingvessel/');
+            $upload_success = $this->upload->do_upload('vesselImage');
+            if($upload_success)
+            {
+                $upload_data = $this->upload->data();
+                // var_dump($upload_data);
+                $image_path = 'upload/' . $upload_data['file_name'];
+                $this->load->model('fishingvessel_model');
+                $this->fishingvessel_model->save_new_vessel($image_path);
+                redirect('fishingvessel/');
+            }
+            else 
+            {
+                $this->load->model('country_model');
+                $result = $this->country_model->get_all();
+                $data['country_list'] = $result;
+                $data['title'] = "เพิ่มข้อมูลเรือประมง";
+                $this->load->view('header', $data);
+                $this->load->view('fishing-vessel/new-ship', $data);
+                $this->load->view('footer');
+            }   
         }
         
     }
