dce54267429c19a5426b393e44b8d6dd9a6153cf
aeksorra <gsorra@gmail.com>
2018-05-07 12:10:08 +0700

    แก้ไขตาม อ.

--- application/controllers/FishingVessel.php
+++ application/controllers/FishingVessel.php
@@ -1,82 +1,62 @@
 <?php
-
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class FishingVessel extends CI_Controller {
-
+defined('BASEPATH') or exit('No direct script access allowed');
+class FishingVessel extends CI_Controller
+{
     public function index()
     {
         $this->load->model('fishingvessel_model');
         $result = $this->fishingvessel_model->get_all();
-
         $data['vessels'] = $result;
         $data['title'] = "รายชื่อเรือประมงทั้งหมด";
-
         $this->load->view('header', $data);
         $this->load->view('fishing-vessel/index', $data);
         $this->load->view('footer');
     }
-
     public function all()
     {
-        echo 'Show all vessels...'; 
+        echo 'Show all vessels...';
     }
-
     public function new_vessel()
     {
         $this->load->model('country_model');
         $result = $this->country_model->get_all();
-
         $data['country_list'] = $result;
         $data['title'] = "เพิ่มข้อมูลเรือประมง";
-
         $this->load->view('header', $data);
         $this->load->view('fishing-vessel/new-ship', $data);
         $this->load->view('footer');
     }
-
     public function create()
     {
         $this->load->library('form_validation');
-        $this->form_validation->set_rules('vesselName', 'vesselName', 'required|max_length[10]', array('required'=>'ช่วยกรอกชื่อสำเภาด้วยน่ะออเจ้า'));
-
-        if($this->form_validation->run() == false)
+        $this->form_validation->set_rules('vesselName', 'ชื่อเรือ', 'required|max_length[10]', array('required' => 'ช่วยกรอกชื่อสำเภาด้วยนะออเจ้า' ));
+        if ($this->form_validation->run() == false) 
         {
             $this->load->model('country_model');
             $result = $this->country_model->get_all();
-
             $data['country_list'] = $result;
             $data['title'] = "เพิ่มข้อมูลเรือประมง";
-
             $this->load->view('header', $data);
             $this->load->view('fishing-vessel/new-ship', $data);
             $this->load->view('footer');
         }
-        else
+        else 
         {
             $this->load->model('fishingvessel_model');
             $this->fishingvessel_model->save_new_vessel();
-
             redirect('fishingvessel/');
-        }      
-
-        // $this->load->model('fishingvessel_model');
-        // $this->fishingvessel_model->save_new_vessel();
-
-        // redirect('fishingvessel/');
+        }
+        
     }
-
     public function new_success()
     {
-
     }
-
-
-
+    public function delete_vessel()
+    {
+        // var_dump($_POST);
+        $this->load->model('fishingvessel_model');
+        $this->fishingvessel_model->delete_vessel();
+        redirect('fishingvessel/');
+    }
 }
-
 /* End of file FishingVessel.php */
-
-
-?>
